6d66df3bf9b61b7a1ee77940eb31135e54e3c6fc
blackkensai <blackkensai@gmail.com>
2018-07-31 15:24:11 +0800

    embed main entry

--- src/node.cc
+++ src/node.cc
@@ -4940,6 +4940,44 @@ void RegisterBuiltinModules() {
 
 }  // namespace node
 
+NODE_EXTERN int node_embed_execute(const char * script) {
+  // atexit([]() { uv_tty_reset_mode(); });
+  int argc = 2;
+  const char ** argv = new const char*[2];
+  argv[0] = "node";
+  argv[1] = script;
+  node::PlatformInit();
+  node::performance::performance_node_start = PERFORMANCE_NOW();
+
+  // This needs to run *before* V8::Initialize().  The const_cast is not
+  // optional, in case you're wondering.
+  int exec_argc;
+  const char** exec_argv;
+  node::Init(&argc, const_cast<const char**>(argv), &exec_argc, &exec_argv);
+
+  node::v8_platform.Initialize(node::v8_thread_pool_size, uv_default_loop());
+  node::V8::Initialize();
+  node::performance::performance_v8_start = PERFORMANCE_NOW();
+  node::v8_initialized = true;
+  const int exit_code =
+      node::Start(uv_default_loop(), argc, argv, exec_argc, exec_argv);
+  node::v8_initialized = false;
+  node::V8::Dispose();
+
+  // uv_run cannot be called from the time before the beforeExit callback
+  // runs until the program exits unless the event loop has any referenced
+  // handles after beforeExit terminates. This prevents unrefed timers
+  // that happen to terminate during shutdown from being run unsafely.
+  // Since uv_run cannot be called, uv_async handles held by the platform
+  // will never be fully cleaned up.
+  node::v8_platform.Dispose();
+
+  delete[] exec_argv;
+  exec_argv = nullptr;
+
+  return exit_code;
+}
+
 #if !HAVE_INSPECTOR
 void InitEmptyBindings() {}
 
